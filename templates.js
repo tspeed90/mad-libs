f296699985949a8a6c73ecce6df5f8088577c74a
tspeed90 <tspeed90@gmail.com>
2018-01-30 23:32:15 +0000

    added four starter template objects to file

---
+++ templates.js
@@ -0,0 +1,19 @@
+let template1 = {
+  template: "Today, I saw him again. When he looks at me with those {{adjective}} eyes, it makes my {{noun}} go pitter patter, and I feel as if I have {{plural noun}} in my stomach. When he scrunches his nose, I want to {{verb}} him softly. He is so {{adjective}} and {{adjective}}! Tomorrow, he will be mine!"
+};
+
+let template2 = {
+  template: "Dear {{person}}," + "\n" +
+  "{{plural noun}} are red, " + "\n" +
+  "{{plural noun}} are blue, " + "\n" +
+  "You love me and I love {{plural noun}}!" + "\n" +
+  "From {{person}}."
+}; 
+
+let template3 = {
+  template: "A vacation is when you take a trip to some {{adjective}} place with your {{adjective}} family. Usually you go to some place that is near a/an {{noun}} or up on a/an {{noun}}. A good vacation place is one where you can ride {{plural noun}} or play {{game}} or go hunting for {{plural noun}}. I like to spend my time {{ing verb}} or {{ing verb}}. When parents go on a vacation, they spend their time eating three {{plural noun}} a day, and fathers play golf, and mothers sit around {{ing verb}}. Last summer, my little brother fell in a/an {{noun}} and got covered in {{noun}}. My family is going to go to {{place}}, and I will practice {{ing verb}}. Parents need vacations more than kids because parents are always very {{adjective}} and because they have to work all year to make enough {{plural noun}} to pay for the vacation."
+};  
+
+let template4 = {
+  template: "Most doctors agree that bicycle {{ing verb}} is a/an {{adjective}} form of exercise. {{ing verb}} a bicycle enables you to develop your {{body part}} muscles as well as {{adverb}} increase the rate of your {{body part}} beat. More {{plural noun}} around the world {{verb}} bicycles than drive {{noun}}. No matter what kind of {{noun}} you {{verb}}, always be sure to wear a/an {{adjective}} helmet. Make sure to have {{color}} reflectors too!"
+};
